fdc6cd97e6c405f9452dafa8e15e4da02bceb0ad
Tihici <tihamer.sebok@gmail.com>
2020-05-21 12:06:21 +0300

    Export class EventModel, delete from events list, add function, #newEventNameInput-template referencia

--- src/app/listdemo/listdemo.component.ts
+++ src/app/listdemo/listdemo.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export class EventModel /*adatleiro modell osztaly*/ {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-listdemo',
   templateUrl: './listdemo.component.html',
   styleUrls: ['./listdemo.component.css']
 })
-export class ListdemoComponent  {
+export class ListdemoComponent {
+  events: EventModel[];
+
 
   constructor() {
-    console.log('megjott a listdemo comp');
+    this.events = [
+      {
+        id: 1,
+        name: 'sziget'
+      },
+      {
+        id: 2,
+        name: 'fezen'
+      },
+      {
+        id: 3,
+        name: 'rockmaraton'
+      }
+    ];
+  }
+
+  add(newEventNameInput) {
+    console.log(newEventNameInput);
+  }
+
+  delete(id: number) {
+    this.events = this.events.filter((ev: EventModel) => ev.id !== id);
   }
 
 }
