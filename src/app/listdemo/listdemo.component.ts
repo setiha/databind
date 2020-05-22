8f8b9db05a6e9973f331ffeb9c1f57bb7bddebd1
Tihici <tihamer.sebok@gmail.com>
2020-05-22 18:04:15 +0300

    'EventModel class  kiszervezese

--- src/app/listdemo/listdemo.component.ts
+++ src/app/listdemo/listdemo.component.ts
@@ -3,6 +3,14 @@ import {Component, OnInit} from '@angular/core';
 export class EventModel /*adatleiro modell osztaly*/ {
   id: number;
   name: string;
+  pic?: string;
+
+  constructor(id, name, pic = ''  ) {
+    //Object.assign- parameterek helyett csak egy parametert varok, param; a masodik objektum elemeit atmasolja az elsore
+    this.id = id;
+    this.name = name;
+    this.pic = pic;
+  }
 }
 
 @Component({
@@ -18,25 +26,31 @@ export class ListdemoComponent {
     this.events = [
       {
         id: 1,
-        name: 'sziget'
+        name: 'sziget',
+        pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSB3gWNOEJv0-ApUKI8-2blHyL0NjHxQUVKOEK5WbZpSgpSivyO&usqp=CAUs'
       },
       {
         id: 2,
-        name: 'fezen'
+        name: 'fezen',
+        pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSB3gWNOEJv0-ApUKI8-2blHyL0NjHxQUVKOEK5WbZpSgpSivyO&usqp=CAUs'
       },
       {
         id: 3,
-        name: 'rockmaraton'
+        name: 'rockmaraton',
+        pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSB3gWNOEJv0-ApUKI8-2blHyL0NjHxQUVKOEK5WbZpSgpSivyO&usqp=CAUs'
       }
     ];
   }
 
-  add(newEventNameInput) {
-    console.log(newEventNameInput);
+  add(newEventNameInput: HTMLInputElement) {
+    //immutability
+    this.events = [...this.events, new EventModel(5, newEventNameInput.value)];
+
+    console.log(newEventNameInput.value);
+    newEventNameInput.value = '';
   }
 
   delete(id: number) {
     this.events = this.events.filter((ev: EventModel) => ev.id !== id);
   }
-
 }
